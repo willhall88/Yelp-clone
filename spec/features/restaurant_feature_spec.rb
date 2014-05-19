f5480094d1c73cb8e350617f5e94867c2fe108e2
Will Hall <willhall88@hotmail.com>
2014-05-19 14:23:48 +0100

    added the first route

---
+++ spec/features/restaurant_feature_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe 'restaurants index page' do
+  context 'no restaurants should have been added' do
+    it 'should display a message' do
+      visit '/restaurants'
+      expect(page).to have_content 'No restaurants yet'
+    end
+  end
+end
