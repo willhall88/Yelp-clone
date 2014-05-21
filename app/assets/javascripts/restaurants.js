ea7b82f0bc45c6c7fca314a4c1212fea8cda7a71
Will Hall <willhall88@hotmail.com>
2014-05-21 18:04:47 +0100

    got a json reply from the review controller

--- app/assets/javascripts/restaurants.js
+++ app/assets/javascripts/restaurants.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
   $('.new_review').on('submit', function(){
     event.preventDefault();
+    $.post($(this).attr('action'), $(this).serialize(), function(response){
+
+      console.log(response)
+
+    }, 'json' );
 
   });
 
-})
+});
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,8 +6,12 @@ class ReviewsController < ApplicationController
 
   def create 
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @restaurant.reviews.create(params[:review].permit(:comments, :rating))
-
-    redirect_to ('/restaurants')
+    review = @restaurant.reviews.create(params[:review].permit(:comments, :rating))
+    
+    respond_to do |format|
+      format.html{ redirect_to '/restaurants' }
+      format.json{ render json: review }
+    end
+    
   end
 end
