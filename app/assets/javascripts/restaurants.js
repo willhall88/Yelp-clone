9f44f1695d83b382a6f8aaf3601e12cd693af944
Will Hall <willhall88@hotmail.com>
2014-05-22 12:56:34 +0100

    added more json to update the review count and the average reviews

--- app/assets/javascripts/restaurants.js
+++ app/assets/javascripts/restaurants.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
   $('.new_review').on('submit', function(){
     event.preventDefault();
-    
-    var siblingList =  $(this).siblings('ul');
 
     $.post($(this).attr('action'), $(this).serialize(), function(response){
         var template = $('#review_template').html();
         var rendered = Mustache.render(template,response);
-        siblingList.prepend(rendered);
-        console.log(response);
+        
+        var targetId = response.restaurant.id;
+        var currentRestaurant = $('.restaurant[data-id=' + targetId + ']');
 
+        currentRestaurant.find('ul').prepend(rendered);
+        
+        currentRestaurant.find('h3').text(response.restaurant.review_count);
+        currentRestaurant.find('h4').text('Average Rating:' + response.restaurant.average_rating_stars + response.restaurant.average_rating);
+             
     }, 'json' );
   });
 });
--- app/views/reviews/create.json.jbuilder
+++ app/views/reviews/create.json.jbuilder
@@ -1,3 +1,9 @@
 json.comments @review.comments
 json.star_rating star_rating(@review.rating)
 
+json.restaurant do
+  json.id @restaurant.id
+  json.review_count pluralize(@restaurant.reviews.count, 'review')
+  json.average_rating @restaurant.average_rating
+  json.average_rating_stars star_rating(@restaurant.average_rating)
+end
